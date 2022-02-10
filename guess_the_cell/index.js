762299a2979333b0bc6f86bd9066c6c59cf5a96e
finder228 <vladmalpro37@gmail.com>
2022-02-10 17:50:43 +0200

    guess the cell demo

--- guess_the_cell/index.js
+++ guess_the_cell/index.js
@@ -1,11 +1,25 @@
+var container = {
+  randNumber: ""
+};
+
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    console.log(Math.floor(Math.random() * (max - min + 1) + min))
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
-  function checkResult(element) {
-    console.log(element.id)
+function checkResult(element) {
 
+  if (element.id == container.randNumber) {
+    alert("you win")
+    container.randNumber = ""
+  } else {
+    console.log("you lose")
   }
+  return element.id
+}
+
+function startGame() {
+  container.randNumber = String(getRandomIntInclusive(1, 10))
+  console.log(container.randNumber)
+} 
